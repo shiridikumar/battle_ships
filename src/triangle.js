095771255b33a737638f89b897862c0a9a2190c8
ShiridiKumar <p.shiridi@research.iiit.ac.in>
2022-02-19 01:30:39 +0530

    started geneerating enemies

--- src/triangle.js
+++ src/triangle.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources
 import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r125/examples/jsm/loaders/DRACOLoader.js";
 import { Sky } from "./Sky.js"
-import { Water} from "./Water.js"
+import { Water } from "./Water.js"
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
@@ -35,9 +35,10 @@ dracoLoader.setDecoderPath('/examples/js/libs/draco/');
 loader.setDRACOLoader(dracoLoader);
 renderer.outputEncoding = THREE.sRGBEncoding;
 // Load a glTF resource
+let loaded=0;
 const loadcont = async () => {
     await loader.load(
-        // resource URL
+        // resource URLgit 
         "./scene.gltf",
         // called when the resource is loaded
         function (gltf) {
@@ -50,6 +51,7 @@ const loadcont = async () => {
             obj.position.y = 0.5;
             obj.position.z = 0;
             renderer.render(scene, camera);
+            loaded=1;
             gltf.animations;
             gltf.scenes;
             gltf.asset;
@@ -125,25 +127,26 @@ function onDocumentKeyDown(event) {
         obj.translateZ(vel);
         camera.translateZ(-vel);
     }
-     else if (keyCode == 83) {
+    else if (keyCode == 83) {
         camera.translateZ(vel);
-    } 
+        obj.translateZ(-vel);
+    }
     else if (keyCode == 65) {
         obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 50);
         obj.translateZ(vel);
-        camera.translateZ(-vel);
-        
+        camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
+
         // camera.lookAt(obj.position);
         // camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 50);
     }
-     else if (keyCode == 68) {
+    else if (keyCode == 68) {
         obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), -vel / 50);
         obj.translateZ(vel);
-        camera.translateZ(-vel);
+        camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
         // camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), -vel / 50)
         // camera.lookAt(obj.position)
     }
-     else if (keyCode == 32) {
+    else if (keyCode == 32) {
         obj.rotateY(-vel / 10);
     }
     render();
@@ -151,23 +154,56 @@ function onDocumentKeyDown(event) {
 
 
 
-// function animate() {
+function animate() {
 
-//     requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-//     render();
 
-// }
+    render();
 
+}
+let ind = 0
+setInterval(async () => {
+    let enemy;
+    console.log("called");
+    console.log(camera.position);
+    if (ind != 1 && loaded==1) {
+        ind=1;
+        await loader.load(
+            "../ship/source/cargo ship.glb",
+            function (gltf) {
+                enemy = gltf.scene;
+                // obj.position.set(0,-50,0);
+                enemy.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+                scene.add(enemy);
+                enemy.position.x = obj.position.x;
+                enemy.position.y = 0.5;
+                enemy.position.z = obj.position.z-6;
+                gltf.animations;
+                gltf.scenes;
+                render();
+                ind=0
+                gltf.asset;
+            },
+            function (xhr) {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+            },
+            function (error) {
+                console.log(error);
+            }
+        );
+    };
+    console.log(obj);
+
+}, 5000);
 
 var render = function () {
-    controls.target.set(obj.position.x, obj.position.y+0.3, obj.position.z);
+    controls.target.set(obj.position.x, obj.position.y + 0.3, obj.position.z);
     controls.update();
-   // camera.position.set(obj.position.x, obj.position.y + 0.5, obj.position.z + 2.5)
+    // camera.position.set(obj.position.x, obj.position.y + 0.5, obj.position.z + 2.5)
     renderer.render(scene, camera);
 };
-
-
 
 
 
