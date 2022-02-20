b618d61308e379e773ba6d009322c3de2e883e58
ShiridiKumar <p.shiridi@research.iiit.ac.in>
2022-02-21 02:22:47 +0530

    updated enemy ships approaching

--- src/triangle.js
+++ src/triangle.js
@@ -23,6 +23,7 @@ var controls = new OrbitControls(camera, renderer.domElement);
 
 
 let obj;
+let enemies = [];
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
 
@@ -30,6 +31,7 @@ scene.add(light);
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/examples/js/libs/draco/');
 loader.setDRACOLoader(dracoLoader);
+let loaded = 0;
 renderer.outputEncoding = THREE.sRGBEncoding;
 // Load a glTF resource
 const loadcont = async () => {
@@ -53,31 +55,29 @@ const loadcont = async () => {
             gltf.asset;
         },
         function (xhr) {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+            //.log((xhr.loaded / xhr.total * 100) + '% loaded');
 
         },
         function (error) {
-            console.log(error);
+            //.log(error);
         }
     );
 }
-
+//
 
 let original;
-let loaded = 0;
-const load_enemies = async() => {
+const load_enemies = async () => {
     await loader.load(
         "../ship/source/cargo ship.glb",
         function (gltf) {
-            original=gltf.scene;
+            original = gltf.scene;
             gltf.asset;
-            loaded=1;
         },
         function (xhr) {
-            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+            //.log((xhr.loaded / xhr.total * 100) + '% loaded');
         },
         function (error) {
-            console.log(error);
+            //.log(error);
         }
     );
 }
@@ -103,13 +103,12 @@ const water = new Water(
 );
 water.rotation.x = - Math.PI / 2;
 scene.add(water);
-
 const waterUniforms = water.material.uniforms;
 
 
 const sky = new Sky();
 sky.scale.setScalar(10000);
-console.log(scene);
+//.log(scene);
 scene.add(sky);
 const skyUniforms = sky.material.uniforms;
 skyUniforms['turbidity'].value = 100;
@@ -136,7 +135,7 @@ scene.environment = pmremGenerator.fromScene(sky).texture;
 loadcont();
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
-let vel = 0.25;
+let vel = 0.125;
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 87) {
@@ -148,7 +147,7 @@ function onDocumentKeyDown(event) {
         obj.translateZ(-vel);
     }
     else if (keyCode == 65) {
-        obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 50);
+        obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 25);
         obj.translateZ(vel);
         camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
 
@@ -157,7 +156,7 @@ function onDocumentKeyDown(event) {
         // camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 50);
     }
     else if (keyCode == 68) {
-        obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), -vel / 50);
+        obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), -vel / 25);
         obj.translateZ(vel);
         camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
         // camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), -vel / 50)
@@ -172,44 +171,81 @@ function onDocumentKeyDown(event) {
 
 
 function animate() {
-
     requestAnimationFrame(animate);
+    if (loaded == 1) {
+        for (var i = 0; i < enemies.length; i++) {
+            var dir = new THREE.Vector3(obj.position.x - enemies[i].position.x, obj.position.y - enemies[i].position.y, obj.position.z - enemies[i].position.z-0.3).normalize();
+            var obj_z=new THREE.Vector3();
+            obj.getWorldDirection(obj_z);
+            enemies[i].position.x += dir.x / 50,
+            enemies[i].position.y += dir.y / 50,
+            enemies[i].position.z += dir.z / 50;
+            var dir = new THREE.Vector3(obj.position.x - enemies[i].position.x, obj.position.y - enemies[i].position.y, obj.position.z - enemies[i].position.z).normalize();
+            let enemy_z = new THREE.Vector3();
+            // obj_z.x*=-1;obj_z.y*=-1;obj_z.z*=-1;
+            enemies[i].getWorldDirection(enemy_z);
+            var enemy_x = new THREE.Vector3().crossVectors(enemy_z, new THREE.Vector3(0, 1, 0))
+            let x_axis = new THREE.Vector3().crossVectors(obj_z, new THREE.Vector3(0, 1, 0));
+            dir.x*=-1;dir.y*=-1;dir.z*=-1;
+            if(x_axis.angleTo(dir)<Math.PI/2){
+                enemies[i].rotateOnAxis(new THREE.Vector3(0, 1, 0), -1 * dir.angleTo(enemy_x)/50); 
+                console.log("left side");
+            }
+            else{
+                enemies[i].rotateOnAxis(new THREE.Vector3(0, 1, 0), (dir.angleTo(enemy_x))/50); 
+                console.log("right side");
+            }
 
-
-    render();
-
+        }
+        render();
+    }
 }
-
+const gen_random = (max, min) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let rotation = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2.0);
 let ind = 0
 setInterval(async () => {
-    console.log("called");
-    console.log(camera.position);
-    for(var i=0;i<5;i++){
+    //.log("called");
+    //.log(camera.position);
+    for (var i = 0; i < 3; i++) {
         if (ind != 1 && loaded == 1) {
             let enemy;
             var x = obj.position.x; var y = obj.position.y; var z = obj.position.z;
-            console.log(x, y, z);
-            console.log(obj.position);
+            //.log(x, y, z);
+            //.log(obj.position);
             let new_ob = new THREE.Vector3(x, y, z);
             obj.getWorldDirection(new_ob);
-
             let x_axis = new THREE.Vector3().crossVectors(new_ob, new THREE.Vector3(0, 1, 0))
-
-            console.log(new_ob);
             enemy = original.clone();
 
-            enemy.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+            //2*Math.PI-dir.angleTo(enemy_x));
             scene.add(enemy);
-            console.log(obj.position, new_ob)
-            enemy.position.x = obj.position.x + 4*new_ob.x+i*x_axis.x;
-            enemy.position.y = obj.position.y + 4*new_ob.y+i*x_axis.y;
-            enemy.position.z = obj.position.z + 4*new_ob.z+i*x_axis.z;
+            //.log(obj.position, new_ob);
+            var pos_z = gen_random(18, 10);
+            var pos_i = gen_random(6, -6);
+            enemy.position.x = obj.position.x + pos_z * new_ob.x + pos_i * x_axis.x;
+            enemy.position.y = obj.position.y + pos_z * new_ob.y + pos_i * x_axis.y;
+            enemy.position.z = obj.position.z + pos_z * new_ob.z + pos_i * x_axis.z;
+
+            var dir = new THREE.Vector3(obj.position.x - enemy.position.x, obj.position.y - enemy.position.y, obj.position.z - enemy.position.z).normalize();
+            let enemy_z = new THREE.Vector3();
+            enemy.getWorldDirection(enemy_z);
+            var enemy_x = new THREE.Vector3().crossVectors(enemy_z, new THREE.Vector3(0, 1, 0))
+            enemy_x.x *= -1; enemy_x.y *= -1; enemy_x.z *= -1
+            enemy.rotateOnAxis(new THREE.Vector3(0, 1, 0), -1 * dir.angleTo(enemy_x))
+            // //.log(dir.angleTo(enemy_x)*57.29);
+            dir.x*=-1;dir.y*=-1;dir.z*=-1;
+
+            console.log(x_axis.angleTo(dir)*57.29);
+            enemies.push(enemy);
             render();
+
             ind = 0;
         }
     }
 
-}, 5000);
+}, 7000);
 
 var render = function () {
     controls.target.set(obj.position.x, obj.position.y + 0.3, obj.position.z);
@@ -220,3 +256,4 @@ var render = function () {
 
 
 
+animate();
