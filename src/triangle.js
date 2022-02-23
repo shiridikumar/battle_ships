397a328587fd15a060e0ddf4bb8b7de9c616c1d2
ShiridiKumar <p.shiridi@research.iiit.ac.in>
2022-02-24 02:10:06 +0530

    almost done

--- src/triangle.js
+++ src/triangle.js
@@ -7,7 +7,7 @@ import { Water } from "./Water.js"
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
-camera.position.set(0, 1, 2.5);
+camera.position.set(0.0, 1.5, 2.0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -18,8 +18,8 @@ var controls = new OrbitControls(camera, renderer.domElement);
 // controls.target.set(0, 0.8, 0);
 // controls.maxDistance = 200.0;
 // controls.update();
-
-
+let treasures=0;
+let kills=0;
 
 
 let obj;
@@ -27,6 +27,7 @@ let score = 0;
 let enemies = [];
 let enemies_health = [];
 let shotsenemy = [];
+let top_view=0;
 let playerhealth = 200;
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
@@ -43,18 +44,18 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 const loadcont = async () => {
     await loader.load(
         // resource URLgit 
-        "./scene.gltf",
+        "./mainship/scene.gltf",
         // called when the resource is loaded
         function (gltf) {
             obj = gltf.scene;
             // obj.position.set(0,-50,0);
+            obj.scale.set(0.06, 0.06, 0.06);
             obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
             obj.translateY(0);
             scene.add(obj);
-            obj.position.x = 0;
+            obj.position.x = 0.0;
             obj.position.y = 0.5;
             obj.position.z = 0;
-
             renderer.render(scene, camera);
             loaded = 1;
             gltf.animations;
@@ -71,7 +72,7 @@ const loadcont = async () => {
         }
     );
 }
-
+// --------------------------------------------------------- cannon load--------------------------------------------------------------------
 let cannon;
 const loadcannon = async () => {
     await loader.load(
@@ -81,9 +82,6 @@ const loadcannon = async () => {
         function (gltf) {
             cannon = gltf.scene
             cannon.scale.set(0.1, 0.1, 0.1);
-            // obj.position.set(0,-50,0);
-            // .rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
-            // obj.translateY(0);
             cannonload = 1;
         },
         function (xhr) {
@@ -98,7 +96,7 @@ const loadcannon = async () => {
 
 loadcannon();
 //
-
+//--------------------------------------------------------------load enemy model----------------------------------------------
 let original;
 const load_enemies = async () => {
     await loader.load(
@@ -118,6 +116,7 @@ const load_enemies = async () => {
 }
 load_enemies();
 
+//-----------------------------------------------------------------------------load chest---------------------------------------------------------
 let chestloaded = 0;
 let originalchest;
 const load_chest = async () => {
@@ -138,12 +137,6 @@ const load_chest = async () => {
     );
 }
 load_chest();
-
-
-
-
-
-
 
 const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
 const water = new Water(
@@ -194,14 +187,64 @@ sky.material.uniforms['sunPosition'].value.copy(sun);
 scene.environment = pmremGenerator.fromScene(sky).texture;
 
 loadcont();
+const checkcollision = () => {
+    var first = new THREE.Box3().setFromObject(obj);
+    if (loaded == 1) {
+        for (var i = 0; i < enemies.length; i++) {
+            var second = new THREE.Box3().setFromObject(enemies[i]);
+            var collision = first.isIntersectionBox(second);
+            if (collision && enemies_health[i]>0) {
+                return true
+            }
+        }
+        return false;
+    }
+}
+
+const collisionenemy = (ship) => {
+    if (loaded == 1) {
+        var first = new THREE.Box3().setFromObject(obj);
+        var second = new THREE.Box3().setFromObject(ship);
+        var collision = first.isIntersectionBox(second);
+        //("enemy vcolllsuion");
+        if (collision) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
+
 document.addEventListener("keydown", onDocumentKeyDown, false);
 let vel = 0.125;
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
+
+    //------------------------------------------------------------W Key-------------------------------------------
     if (keyCode == 87) {
         obj.translateZ(vel);
-        camera.translateZ(-vel);
+        if(top_view==0){
+            camera.translateZ(-vel);
+            console.log("translating");
+        }
+        else{
+            camera.translateY(vel);
+        }
+        if (checkcollision()) {
+            obj.translateZ(-vel);
+            if(top_view==0){
+                camera.translateZ(vel);
+            }
+            else{
+                camera.translateY(-vel);
+            }
+            //("helllllo");
+        };
     }
+
+
+//-------------------------------------------------------------S Key-------------------------------------------------
     else if (keyCode == 83) {
         obj.translateZ(-vel);
         var obj_z_dir = new THREE.Vector3();
@@ -209,46 +252,121 @@ function onDocumentKeyDown(event) {
         camera.position.x -= obj_z_dir.x * vel;
         camera.position.y -= obj_z_dir.y * vel;
         camera.position.z -= obj_z_dir.z * vel;
-
+        if(checkcollision()){
+            obj.translateZ(vel);
+            camera.position.x += obj_z_dir.x * vel;
+            camera.position.y += obj_z_dir.y * vel;
+            camera.position.z += obj_z_dir.z * vel; 
+        }
     }
+
+// ---------------------------------------------------------A Key -------------------------------------------------
     else if (keyCode == 65) {
         obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 25);
         obj.translateZ(vel);
-        camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
+        // camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
+        if(top_view==0){
+           camera.translateZ( -vel);
+        }
+        else{
+            camera.translateY( vel);
+        }
+        if(checkcollision()){
+            obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / -25);
+            obj.translateZ(-vel);
+            if(top_view==0){
+                camera.translateZ( vel);
+            }
+            else{
+                camera.translateY(-vel);
+            }
+        }
     }
-
+// -----------------------------------------------------------------------D Key---------------------------------------------------------
     else if (keyCode == 68) {
         obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), -vel / 25);
         obj.translateZ(vel);
-        camera.translateOnAxis(new THREE.Vector3(0, 0, 1), -vel);
+        if(top_view==0){
+            camera.translateZ( -vel);
+        }
+        else{
+            camera.translateY(vel);
+        }
+        if(checkcollision()){
+            obj.rotateOnAxis(new THREE.Vector3(0, 1, 0), vel / 25);
+            obj.translateZ(-vel);
+            if(top_view==0){
+                camera.translateZ(vel);
+            }
+            else{
+                camera.translateY( -vel);
+            }
+        }
 
     }
+// ----------------------------------------------------------------------Top Vieew Key----------------------------------------
     else if (keyCode == 32) {
-        obj.rotateY(-vel / 10);
+        // obj.rotateY(-vel / 10);
+        if(top_view==0){
+            camera.position.x=obj.position.x;camera.position.y=obj.position.y;
+            camera.position.z=obj.position.z;
+            console.log(camera.rotation);
+            camera.rotation.x=Math.PI/2;
+            // camera.lookAt(new THREE.Vector3(0,1,0));
+            // camera.lookAt(0,1,0);
+            console.log(camera.getWorldDirection(new THREE.Vector3()));
+            camera.position.y+=4;
+            // camera.rotation.x=Math.PI/2;camera.rotation.y=0;camera.rotation.z=0;
+            // camera.position.x=obj.position.x;
+            // camera.position.z=obj.position.z;
+            
+            top_view=1;
+            //(camera.position);
+        }
+        else{
+            var dir=new THREE.Vector3();
+            obj.getWorldDirection(dir);
+            camera.position.x=obj.position.x-2.5*dir.x;
+            camera.position.y=2.5;
+            camera.position.z=obj.position.z-2.5*dir.z;
+            camera.rotation.x=0;
+            top_view=0;
+        }
+        console.log(camera.getWorldDirection(new THREE.Vector3()));
+        console.log(camera.position,obj.position);
     }
-
+//-------------------------------------------------------- Cannon shot Key ----------------------------------------------------------
     else if (keyCode == 66) {
-        if (cannonload == 1) {
-            console.log("cannon shot");
+        if (cannonload == 1 && playerhealth>0) {
+            //("cannon shot");
             cannonshot(obj);
             // cannon.position.x=obj.position.x+0.4;cannon.position.y=obj.position.y;cannon.position.z=obj.position.z;
         }
     }
+// -----------------------------------------------   zoom Out ----------------------------------------------------------------------------------------
     else if (keyCode == 88) {
         camera.translateZ(vel);
-
     }
     render();
 };
 
 
-
+//-----------------------------------------------------Animate-----------------------------------------------------------------------------------
 function animate() {
     requestAnimationFrame(animate);
+    if(playerhealth<=0){
+        scene.remove(obj);
+        renderer.render(scene, camera);
+        return 1;
+    }
     if (loaded == 1) {
         for (var i = 0; i < enemies.length; i++) {
-            if (enemies_health[i] <= 0) {
+            var ans=collisionenemy(enemies[i])
+            if (enemies_health[i] <= 0 ) {
                 scene.remove(enemies[i]);
+                continue;
+            }
+            if(ans){
                 continue;
             }
             var dir = new THREE.Vector3(obj.position.x - enemies[i].position.x, obj.position.y - enemies[i].position.y, obj.position.z - enemies[i].position.z - 0.3).normalize();
@@ -286,9 +404,14 @@ function animate() {
                     var collision = first.isIntersectionBox(second);
                     if (collision && enemies_health[j] > 0) {
                         enemies_health[j] -= 10;
+                        score+=2;
                         scene.remove(shots[i][0]);
                         shots[i][2] = -1;
-                        console.log(j, enemies_health[j]);
+                        //(j, enemies_health[j]);
+                        if(enemies_health[j]<=0){
+                            score+=10;
+                            kills+=1;
+                        }
                         break;
                     }
                 }
@@ -299,7 +422,7 @@ function animate() {
             }
         }
         for (var i = 0; i < shotsenemy.length; i++) {
-            if (shotsenemy[i][2] > 0 && enemies_health[shotsenemy[i][3]]>0) {
+            if (shotsenemy[i][2]  > 0) {
                 var zaxis = new THREE.Vector3();
                 shotsenemy[i][1].getWorldDirection(zaxis);
                 shotsenemy[i][0].position.x += zaxis.x / 2;
@@ -313,9 +436,10 @@ function animate() {
                 if (playerhealth > 0 && collision) {
                     playerhealth -= 10;
                     shotsenemy[i][2] = -1;
-                    console.log(playerhealth);
+                    //(playerhealth);
                     if (playerhealth <= 0) {
                         scene.remove(obj);
+                        console.log(score,kills,treasures);
                     }
                 }
             }
@@ -333,18 +457,26 @@ function animate() {
                 var collision = first.isIntersectionBox(second);
                 if (collision && playerhealth > 0) {
                     score += 20;
+                    treasures+=1;
                     scene.remove(chest_array[i][0]);
-                    console.log(score);
-                    chest_array[i][1]=1;
+                    //(score);
+                    chest_array[i][1] = 1;
                 }
             }
         }
+
         render();
     }
 }
+
+//------------------------------------------------------------------Random integer function------------------------------------------------------------
+
 const gen_random = (max, min) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+//--------------------------------------------------------------------- set interval for Enemy and treasure generation-----------------------------------------------
 let rotation = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2.0);
 let ind = 0
 let num_enemies = 0;
@@ -385,49 +517,62 @@ setInterval(async () => {
             loaded = 1;
 
             // cannonshot_enemy(enemy);
-            console.log(x_axis.angleTo(dir) * 57.29);
+            //(x_axis.angleTo(dir) * 57.29);
             enemies.push(enemy);
             enemies_health.push(30);
             num_enemies += 1;
             render();
             ind = 0;
+            if(chest_array.length<20){
 
-            let chest;
-            chest=originalchest.clone();
-            pos_z = gen_random(18, 10);
-            pos_i = gen_random(6, -6);
-            chest.position.x = obj.position.x + pos_z * new_ob.x + pos_i * x_axis.x;
-            chest.position.y = obj.position.y + pos_z * new_ob.y + pos_i * x_axis.y;
-            chest.position.z = obj.position.z + pos_z * new_ob.z + pos_i * x_axis.z;
-            scene.add(chest);
-            chest_array.push([chest, 0]);
+                let chest;
+                chest = originalchest.clone();
+                pos_z = gen_random(18, 10);
+                pos_i = gen_random(6, -6);
+                chest.position.x = obj.position.x + pos_z * new_ob.x + pos_i * x_axis.x;
+                chest.position.y = obj.position.y + pos_z * new_ob.y + pos_i * x_axis.y;
+                chest.position.z = obj.position.z + pos_z * new_ob.z + pos_i * x_axis.z;
+                scene.add(chest);
+                chest_array.push([chest, 0]);
+            }
         }
     }
 
 }, 10000);
 
-
-    setInterval(async () => {
-        //.log("called");
-        //.log(camera.position);
-        for (var i = 0; i < enemies.length; i++) {
-            var random = gen_random(5, 1);
-            console.log('hellllllo');
-            if (random == 1) {
-                cannonshot_enemy(enemies[i],i);
+// ---------------------------------------------------------------------------------- set interval for cannonshot enemy----------------------------------------------------
+setInterval(async () => {
+    //.log("called");
+    //.log(camera.position);
+    for (var i = 0; i < enemies.length; i++) {
+        var random = gen_random(5, 1);
+        //('hellllllo');
+        if (random == 1) {
+            if(enemies_health[i]>0){
+                cannonshot_enemy(enemies[i], i);
             }
         }
-    }, 2000);
+    }
+}, 2000);
 
-
+//-----------------------------------------------------------render function--------------------------------------------------------------------
 
 var render = function () {
-    controls.target.set(obj.position.x, obj.position.y + 0.3, obj.position.z);
-    controls.update();
+    if(playerhealth>0){
+        controls.target.set(obj.position.x, obj.position.y+0.5, obj.position.z);
+        controls.update();
     // camera.position.set(obj.position.x, obj.position.y + 0.5, obj.position.z + 2.5)
-    renderer.render(scene, camera);
+        renderer.render(scene, camera);
+    }
+    else{
+        scene.remove(obj);
+        renderer.render(scene, camera);
+
+    }
 };
 
+
+//--------------------------------------------------------------------cannon shot main ship----------------------------------------------------
 
 const cannonshot = (ship) => {
     if (cannonload == 1) {
@@ -435,7 +580,7 @@ const cannonshot = (ship) => {
         var obj_z = new THREE.Vector3();
         ship.getWorldDirection(obj_z);
         var zaxis = new THREE.Vector3(1, 0, 0);
-        console.log(zaxis.angleTo(obj_z) * 57.29);
+        //(zaxis.angleTo(obj_z) * 57.29);
         // dup.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0),-1*(Math.PI/2-zaxis.angleTo(obj_z)));
 
         let new_ob = new THREE.Vector3();
@@ -451,17 +596,19 @@ const cannonshot = (ship) => {
 }
 
 
+//----------------------------------------------------------------------------cannon shot enemy ship-------------------------------------------------------
+
 const cannonshot_enemy = (ship, i) => {
-    if (cannonload == 1) {
+    if (cannonload == 1 && enemies_health[i]>0) {
         var dup = cannon.clone();
         var obj_z = new THREE.Vector3();
         ship.getWorldDirection(obj_z);
         var zaxis = new THREE.Vector3(1, 0, 0);
-        console.log(zaxis.angleTo(obj_z) * 57.29);
+        //(zaxis.angleTo(obj_z) * 57.29);
         // dup.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0),-1*(Math.PI/2-zaxis.angleTo(obj_z)));
         let new_ob = new THREE.Vector3();
         dup.getWorldDirection(new_ob);
-        console.log("enemyshot");
+        //("enemyshot");
         let x_axis = new THREE.Vector3().crossVectors(new_ob, new THREE.Vector3(0, 1, 0))
         let objx = new THREE.Vector3().crossVectors(obj_z, new THREE.Vector3(0, 1, 0))
         dup.position.x = ship.position.x - x_axis.x * 0.4 + new_ob.x * 0.75//+ obj_z.x*1//-0.4*objx.x;
